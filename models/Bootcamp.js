d38e5aec2fc2dc601a131fe1faed6c55cf827037
Ashish0077 <ashish007722@gmail.com>
2020-03-09 04:52:33 +0530

    adding the slug field using mongoose middleware adding slug field to the document before saving, the slug field is computed using slugify library
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -109,6 +110,14 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+
+// create bootcmap slug from the name
+BootcampSchema.pre("save", function(next) {
+  this.slug = slugify(this.name, {
+    lower: true
+  });
+  next();
 });
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
