ba718a049ee7d9458de206c8055b5288371c9ca3
Ashish0077 <ashish007722@gmail.com>
2020-03-11 17:09:05 +0530

    update seeder now it will import and destroy courses data also
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({
 });
 
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 
 // connect to our db
 mongoose.connect(process.env.MONGO_URI, {
@@ -24,10 +25,15 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
 
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
+
 // import into db
 async function importData() {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log("Data Improted...".green.inverse);
     process.exit(0);
   } catch (error) {
@@ -39,6 +45,7 @@ async function importData() {
 async function deleteData() {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log("Data destroyed...".red.inverse);
     process.exit(0);
   } catch (error) {
