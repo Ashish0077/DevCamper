cca126b3c3d3ee2784998df9347fdd0914002df9
Ashish0077 <ashish007722@gmail.com>
2020-03-12 15:59:16 +0530

    populating bootcamp field courses are requested
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

--- controllers/courses.js
+++ controllers/courses.js
@@ -14,7 +14,10 @@ const getAllCourses = asyncHandler(async (req, res, next) => {
   if (req.params.bootcampId) {
     query = Course.find({ bootcamp: req.params.bootcampId });
   } else {
-    query = Course.find({});
+    query = Course.find({}).populate({
+      path: "bootcamp",
+      select: "name description"
+    });
   }
 
   const courses = await query;
