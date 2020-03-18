447cbce7a6049b031975be6b5173e7322d0a5369
Ashish0077 <ashish007722@gmail.com>
2020-03-18 20:13:12 +0530

    UserSchema mongoose model
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name"]
+  },
+  email: {
+    type: String,
+    required: [true, "Please add email"],
+    unique: [true, "User already exist"],
+    match: [
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please Enter a valid email address"
+    ]
+  },
+  role: {
+    type: String,
+    enum: ["user", "publisher"],
+    default: "user"
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+    minlength: 6,
+    select: false
+  },
+  resetPasswordToken: String,
+  resetPasswordTokenExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+module.exports = mongoose.model("User", UserSchema);
