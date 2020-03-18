61e0a6594634eadad81cce96840da58f552a07e3
Ashish0077 <ashish007722@gmail.com>
2020-03-18 21:38:00 +0530

    registering User creating User in DB and Hashing the Password
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,20 @@ const User = require("../models/User");
     @access  Public
 */
 const registerUser = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  // creating the user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  });
+
+  if (!user) {
+    return next(new ErrorResponse("Unable to create User", 500));
+  }
+
   res.status(200).json({
     success: true
   });
