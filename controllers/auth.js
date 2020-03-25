c30b6fa28463b79d09fae28e6443a248f108a29e
Ashish0077 <ashish007722@gmail.com>
2020-03-25 05:33:06 +0530

    reset password emails using mailtrap smtp
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/asyncHandler");
 const User = require("../models/User");
+const sendEmail = require("../utils/sendEmail");
 
 /* 
     @desc    Register User
@@ -68,13 +69,13 @@ const getMe = asyncHandler(async (req, res, next) => {
   });
 });
 
-  /* 
+/* 
       @desc    reset password
       @route   POST /api/v1/auth/forgetpassword
       @access  Private
   */
 const forgotPassword = asyncHandler(async (req, res, next) => {
-  const user = await User.findOne({ email: req.body.email});
+  const user = await User.findOne({ email: req.body.email });
 
   if (!user) {
     return next(new ErrorResponse("There is no user with this email", 404));
@@ -83,7 +84,35 @@ const forgotPassword = asyncHandler(async (req, res, next) => {
   // get reset method
   const resetToken = user.getResetPasswordToken();
 
-  await user.save({ validateBeforeSave: false })
+  await user.save({ validateBeforeSave: false });
+
+  // create reset URL
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you (or someone else) has requested the reset of a password, Please make a PUT request to \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Password Reset",
+      message
+    });
+
+    res.status(200).json({
+      success: true,
+      data: "Email Sent"
+    });
+  } catch (error) {
+    console.log(error);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse("Email Could not be sent", 500));
+  }
 
   res.status(200).json({
     success: true,
---
+++ utils/sendEmail.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async options => {
+  // create reusable transporter object
+  let transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_EMAIL,
+      pass: process.env.SMTP_PASSWORD
+    }
+  });
+
+  // send mail with transport object
+  const message = {
+    from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message // plain text body
+  };
+
+  const info = await transporter.sendMail(message);
+
+  console.log("Message sent: %s", info.messageId);
+};
+
+module.exports = sendEmail;
